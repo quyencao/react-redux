54aa7385972bd3d12f00d97c84596c57acd77b83
quyencao <boy20195@gmail.com>
2017-03-18 23:25:14 +0700

    Init commit

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,3 @@
+/**
+ * Created by LaptopTCC on 3/18/2017.
+ */
---
+++ client/reduxstagram.js
@@ -0,0 +1,11 @@
+// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+import Main from './components/Main';
+
+// import css
+import css from './styles/style.styl'
+
+render(<Main/>, document.getElementById('root'));
